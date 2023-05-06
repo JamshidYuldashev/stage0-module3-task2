6113159f9d4c0fc66b0867b2f8b06064f036a443
@JamshidYuldashev <jamshidyuldashev7799@gmail.com>
2023-05-06 20:17:56 +0500

    update variable name

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,14 +3,14 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable1 = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable2 = 4000;
+        int variable2 = 2000;
+        int value1 = 1000;
+        int variable3 = 4000;
         int value2 = 3000;
-        int variables = 5000;
+        int variable4 = 5000;
         int sum1 = 7000;
         int sum2 = 90000;
-        int number = 100000;
+        int number1 = 100000;
         int myInteger1 = 1000000;
         int myVar = 1234567;
         int myInteger = 1234567;
